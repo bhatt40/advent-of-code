3b25c9e971edb43ea15de4d0c7cddf58983419f7
Rishee Bhatt <bhatt.40@gmail.com>
2020-12-10 10:36:33 -0600

    2019 day 10 part 2.

--- 2019/day-10/day-10.py
+++ 2019/day-10/day-10.py
@@ -1,5 +1,6 @@
 from fractions import Fraction
-
+from operator import itemgetter
+from math import atan2, degrees
 
 def get_asteroid_coordinates(map):
     asteroids = set()
@@ -32,9 +33,10 @@ def get_asteroids_between_origin_and_other_asteroids(origin, asteroids):
     return asteroids_between_lookup_dict
 
 
-def get_points_between_two_points(p1, p2):
+def calculate_slope_between_two_points(p1, p2):
     (x1, y1) = p1
     (x2, y2) = p2
+
     if x1 == x2:
         slope = [0, 1]
     elif y1 == y2:
@@ -45,6 +47,16 @@ def get_points_between_two_points(p1, p2):
 
     slope[0] = (-1 * abs(slope[0])) if x2 < x1 else abs(slope[0])
     slope[1] = (-1 * abs(slope[1])) if y2 < y1 else abs(slope[1])
+
+    slope = tuple(slope)
+
+    return slope
+
+
+def get_points_between_two_points(p1, p2):
+    (x1, y1) = p1
+    (x2, y2) = p2
+    slope = calculate_slope_between_two_points(p1, p2)
 
     x = x1
     y = y1
@@ -69,6 +81,64 @@ def count_detectable_asteroids(asteroids_between_lookup_dict):
     return count
 
 
+def group_asteroids_between_by_angle(origin, asteroids_between_lookup_dict):
+    slopes_dict = {}
+    for asteroid, asteroids_between in asteroids_between_lookup_dict.items():
+        slope = calculate_slope_between_two_points(origin, asteroid)
+        asteroid_item = (asteroid, len(asteroids_between))
+        if slope in slopes_dict:
+            slopes_dict[slope].append(asteroid_item)
+        else:
+            slopes_dict[slope] = [asteroid_item]
+
+    # Sort each slope's items by number of asteroids between and convert slope to angle
+    slopes_dict = {
+        calculate_angle_of_slope(slope): sorted(items, key=itemgetter(1))
+        for slope, items in slopes_dict.items()
+    }
+
+    return slopes_dict
+
+
+def calculate_angle_of_slope(slope):
+    (x, y) = slope
+
+    # Angles are calculated as clockwise from 90 degrees since laser starts at 12 o'clock
+    return (270 - degrees(atan2(y, x))) % 360
+
+
+def vaporize_next_asteroid_in_list(vaporized_set, list_of_asteroids_at_angle):
+    for asteroid_item in list_of_asteroids_at_angle:
+        asteroid = asteroid_item[0]
+        if asteroid not in vaporized_set:
+            return asteroid
+
+    return None
+
+
+def vaporize_n_asteroids(start_angle, n, sorted_angles, grouped_asteroids):
+    laser_index = sorted_angles.index(start_angle)
+    vaporized_set = set()
+    last_vaporized_android = None
+
+    while len(vaporized_set) < n:
+        angle = sorted_angles[laser_index]
+        vaporized_asteroid = vaporize_next_asteroid_in_list(vaporized_set, grouped_asteroids[angle])
+        if vaporized_asteroid:
+            vaporized_set.add(vaporized_asteroid)
+            last_vaporized_android = vaporized_asteroid
+        else:
+            print('Skipped {}'.format(angle))
+
+        laser_index += 1
+        laser_index = laser_index % len(sorted_angles)
+
+    return last_vaporized_android
+
+
+LASER_START_ANGLE = 0
+NUMBER_OF_ASTEROIDS_TO_VAPORIZE = 200
+
 with open('input.txt', 'r') as f:
     map = f.readlines()
 
@@ -87,7 +157,12 @@ detectable_asteroid_counts = {
 max_detectable_asteroids = max(detectable_asteroid_counts.keys())
 print(max_detectable_asteroids)
 
-# Part
+# Part 2
 station_location = detectable_asteroid_counts[max_detectable_asteroids]
-print(station_location)
+station_asteroids_between_lookup_dict = all_asteroids_between_lookup_dict[station_location]
 
+grouped_by_angles = group_asteroids_between_by_angle(station_location, station_asteroids_between_lookup_dict)
+angles = list(grouped_by_angles.keys())
+angles.sort(reverse=True)
+
+print(vaporize_n_asteroids(LASER_START_ANGLE, NUMBER_OF_ASTEROIDS_TO_VAPORIZE, angles, grouped_by_angles))
