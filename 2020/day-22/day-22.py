e6751eb9b224047d03a2381871fe61b662407fd0
Rishee Bhatt <bhatt.40@gmail.com>
2020-12-30 19:57:08 -0600

    2020 day 22 part 2.

--- 2020/day-22/day-22.py
+++ 2020/day-22/day-22.py
@@ -14,7 +14,19 @@ def play_round(q1, q2):
         q2.put(c1)
 
 
-def play_recursive_round(q1, q2, previous_scores):
+def play_recursive_game(q1, q2, game_number):
+    winner = None
+    previous_scores = set()
+    while not q1.empty() and not q2.empty():
+        winner = play_recursive_round(q1, q2, previous_scores, game_number)
+        if not winner:
+            break
+
+    return winner or 1
+
+
+def play_recursive_round(q1, q2, previous_scores, game_number):
+    print(game_number)
     s1 = calculate_score(q1)
     s2 = calculate_score(q2)
     if (s1, s2) in previous_scores:
@@ -27,7 +39,7 @@ def play_recursive_round(q1, q2, previous_scores):
     if q1.qsize() >= c1 and q2.qsize() >= c2:
         q1_copy = create_subgame_deck(q1, c1)
         q2_copy = create_subgame_deck(q2, c2)
-        winner = play_recursive_round(q1_copy, q2_copy, previous_scores)
+        winner = play_recursive_game(q1_copy, q2_copy, game_number + 1)
     elif c1 > c2:
         winner = 1
     else:
@@ -72,7 +84,7 @@ def calculate_score(q):
 q1_original = Queue()
 q2_original = Queue()
 
-with open('test.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     f.readline()
     while True:
         card = f.readline()
@@ -103,8 +115,10 @@ with open('test.txt', 'r') as f:
 q1 = copy(q1_original)
 q2 = copy(q2_original)
 score_set = set()
-while not q1.empty() and not q2.empty():
-    play_recursive_round(q1, q2, score_set)
 
-print(calculate_score(q1))
-print(calculate_score(q2))
+winner = play_recursive_game(q1, q2, 0)
+if winner == 2:
+    print(calculate_score(q2))
+else:
+    print(calculate_score(q1))
+
