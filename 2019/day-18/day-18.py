234b228d862c0b47c1b520534479733284dfe17d
Rishee Bhatt <rishee.bhatt@woodmac.com>
2021-01-19 19:38:23 -0600

    Intermediate commit.

--- 2019/day-18/day-18.py
+++ 2019/day-18/day-18.py
@@ -3,7 +3,6 @@ import re
 from math import inf
 from functools import lru_cache
 from collections import defaultdict
-import time
 
 
 def build_graph(grid):
@@ -62,46 +61,51 @@ def find_adjacents(grid, start):
 
 
 @lru_cache(maxsize=None)
-def next_available_keys(current_position, keys_in_hand):
-    keys = {}
+def next_available_keys(current_positions, keys_in_hand):
+    keys = []
     distances = defaultdict(lambda: inf)
     q = Queue()
-    q.put((current_position, 0))
+    for index, current_position in enumerate(current_positions):
+        q.put((current_position, 0, index))
 
-    def add_new_positions_to_queue(items):
+    def add_new_positions_to_queue(items, index):
         for new_pos, new_dist in items:
             if dist + new_dist < distances[new_pos]:
                 distances[new_pos] = dist + new_dist
-                q.put((new_pos, dist + new_dist))
+                q.put((new_pos, dist + new_dist, index))
 
     while not q.empty():
-        pos, dist = q.get()
+        pos, dist, index = q.get()
         if 'a' <= pos <= 'z':
-            if pos not in keys_in_hand and pos != current_position:
-                keys[pos] = dist
+            if pos not in keys_in_hand and pos != current_positions[index]:
+                keys.append((pos, dist, index))
             else:
-                add_new_positions_to_queue(graph[pos].items())
+                add_new_positions_to_queue(graphs[index][pos].items(), index)
         elif 'A' <= pos <= 'Z':
             if pos.lower() not in keys_in_hand:
                 continue
             else:
-                add_new_positions_to_queue(graph[pos].items())
+                add_new_positions_to_queue(graphs[index][pos].items(), index)
         else:
-            add_new_positions_to_queue(graph[pos].items())
+            add_new_positions_to_queue(graphs[index][pos].items(), index)
 
     return keys
 
 
 @lru_cache(maxsize=None)
-def minimum_steps(pos, num_to_find, keys_in_hand):
+def minimum_steps(positions, num_to_find, keys_in_hand):
     if num_to_find == 0:
         return 0
 
     min_dist = inf
 
-    for new_pos, dist in next_available_keys(pos, keys_in_hand).items():
+    for new_pos, dist, index in next_available_keys(positions, keys_in_hand):
         new_keys_in_hand = frozenset(keys_in_hand | {new_pos})
-        dist += minimum_steps(new_pos, num_to_find - 1, new_keys_in_hand)
+        positions = tuple([
+            new_pos if i == index else p
+            for i, p in enumerate(positions)
+        ])
+        dist += minimum_steps(positions, num_to_find - 1, new_keys_in_hand)
 
         if dist < min_dist:
             min_dist = dist
@@ -115,10 +119,11 @@ with open('input.txt', 'r') as f:
     ]
 
 graph, starting_position = build_graph(grid)
+graphs = [graph]
 
 number_of_keys = len([
     key for key in graph.keys() if 'a' <= key <= 'z'
 ])
 
 # Part 1
-print(minimum_steps('@', number_of_keys, frozenset()))
+print(minimum_steps(('@',), number_of_keys, frozenset()))
