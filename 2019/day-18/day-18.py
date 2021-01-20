a4fc34035dfd432248d7bd3c8662e4d835cf6955
Rishee Bhatt <rishee.bhatt@woodmac.com>
2021-01-20 13:05:27 -0600

    Refactored.

--- 2019/day-18/day-18.py
+++ 2019/day-18/day-18.py
@@ -1,59 +1,8 @@
 from queue import Queue
-import re
 from math import inf
 from functools import lru_cache
 from collections import defaultdict
-
-
-def build_graph(grid):
-    graph = {}
-
-    for r, row in enumerate(grid):
-        for c, col in enumerate(row):
-            if col not in '.#':
-                graph[col] = find_adjacents(grid, (r, c))
-
-    return graph
-
-
-def get_neighbors(grid, node):
-    r, c = node
-    for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        r2 = r + dr
-        c2 = c + dc
-
-        if 0 <= r2 < len(grid) and 0 <= c2 < len(grid[r2]):
-            if grid[r2][c2] != '#':
-                yield r2, c2
-
-
-def find_adjacents(grid, start):
-    visited = {start}
-    adjacents = {}
-
-    q = Queue()
-    q.put((0, start))
-
-    while not q.empty():
-        dist, node = q.get()
-        n_dist = dist + 1
-
-        for neighbor in get_neighbors(grid, node):
-            if neighbor in visited:
-                continue
-
-            r, c = neighbor
-            value = grid[r][c]
-
-            if re.match(r'[a-zA-Z]', value):
-                adjacents[value] = min(n_dist, adjacents[value]) if value in adjacents else n_dist
-                continue
-
-            q.put((n_dist, neighbor))
-
-        visited.add(node)
-
-    return adjacents
+from node_graph import build_graph
 
 
 @lru_cache(maxsize=None)
